7b9b5197e30f023d5ea29dfd99431754812c6642
Geoffrey Omollo <geoffreyomollo001@gmail.com>
2024-06-27 21:48:49 +0300

    Added Categories controller

---
+++ src/FC.Codeflix.Catalog.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,96 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Category;
+using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FC.Codeflix.Catalog.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(
+        ILogger<CategoriesController> logger,
+        IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ApiResponse<CategoryModelOutput>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    public async Task<IActionResult> Create([FromBody] CreateCategoryInput request, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(request, cancellationToken);
+        return CreatedAtAction(nameof(Create), new { result.Id }, new ApiResponse<CategoryModelOutput>(result));
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(ApiResponse<CategoryModelOutput>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetCategoryInput(id), cancellationToken);
+        return Ok(new ApiResponse<CategoryModelOutput>(result));
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(typeof(ApiResponse<CategoryModelOutput>), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(new DeleteCategoryInput(id), cancellationToken);
+        return NoContent();
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(ApiResponse<CategoryModelOutput>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    public async Task<IActionResult> PutById(
+        [FromRoute] Guid id,
+        [FromBody] UpdateCategoryApiInput apiInput,
+        CancellationToken cancellationToken)
+    {
+        var input = new UpdateCategoryInput(id, apiInput.Name, apiInput.Description, apiInput.IsActive);
+
+        var output = await _mediator.Send(input, cancellationToken);
+        return Ok(new ApiResponse<CategoryModelOutput>(output));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ApiResponseList<CategoryModelOutput>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll(
+        CancellationToken cancellationToken,
+        [FromQuery] int? page = null,
+        [FromQuery(Name = "per_page")] int? perPage = null,
+        [FromQuery] string? search = null,
+        [FromQuery] string? sort = null,
+        [FromQuery] SearchOrder? dir = null
+    )
+    {
+        var input = new ListCategoriesInput();
+        if (page is not null) input.Page = page.Value;
+        if (perPage is not null) input.PerPage = perPage.Value;
+        if (string.IsNullOrEmpty(search) is false) input.Search = search;
+        if (string.IsNullOrEmpty(sort) is false) input.Sort = sort;
+        if (dir is not null) input.Dir = dir.Value;
+
+        var result = await _mediator.Send(input, cancellationToken);
+
+        var response = new ApiResponseList<CategoryModelOutput>(result);
+
+        return Ok(response);
+    }
+}
